3e433cafc6aa9bb689d501779e73a856e4a29fb7
Kangjae Choi <awpe89@gmail.com>
2023-09-23 02:00:14 +0900

    Add array length code

--- remove.sh
+++ remove.sh
@@ -5,8 +5,9 @@ cd $HOME/Applications || ! echo "Change Applications Directory Failed."
 cd Chrome\ Apps.localized || ! echo "Change Chrome Apps Directory Failed."
 
 appNames=("Gmail.app" "Google 드라이브.app" "YouTube.app" "문서.app" "스프레드시트.app" "프레젠테이션.app")
+appNamesLength=${#appNames[@]}
 
-for (( i = 0; i < 6; i++ ));
+for (( i = 0; i < ${appNamesLength}; i++ ));
 do
   if [[ -e ${appNames[$i]} ]];
   then
